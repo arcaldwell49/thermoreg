d991e8d19483d38a96d1b5e109884fa0a44e3359
arcaldwell49 <arcaldwell49@gmail.com>
2022-03-11 14:37:35 -0500

    Add dew point calc

--- R/press.R
+++ R/press.R
@@ -5,6 +5,10 @@
 #' @param est a numeric value representing the measurement of interest.
 #' @param type the type of pressure calculation.
 #' @param units the units of measurement. Options include torr, atm, mbar (millibar), and psi. Default is torr.
+#' @param temp the unit of measure for temperatuer. Default is celsius.
+#' @param tdp Dew point temperature
+#' @param tdb Dry bulb temperature
+#' @param rh Relative humiditiy in percentage units (e.g., 40 not .4)
 #'
 #' @describeIn pressure_calcs Pressure measurement
 #' @export
@@ -31,4 +35,98 @@ press_init = function(est = NULL,
                  units = units),
             class = "thermoreg_meas")
 
+
+
 }
+
+#' @describeIn pressure_calcs Calculate actual vapor pressure
+#' @export
+avp_calc = function(tdp = NULL,
+                    tdb = NULL,
+                    rh = NULL,
+                    temp = thermoreg_options("temp_scale"),
+                    units = thermoreg_options("press_scale")){
+
+  if(class(tdp) == "thermoreg_meas"){
+    tdp = tdp$est
+  }
+
+  if(class(tdb) == "thermoreg_meas"){
+    tdb = tdb$est
+  }
+
+  if(class(rh) == "thermoreg_meas"){
+    rh = rh$est
+  }
+
+  if(temp != "celsisus" | temp != "c"){
+    if(!is.null(tdp)){
+      tdp = temp_convert(x = tdp,
+                         to = "celsius",
+                         from = temp,
+                         type = "tdp")$est
+    }
+
+    if(!is.null(tdb)){
+      tdp = temp_convert(x = tdp,
+                         to = "celsius",
+                         from = temp,
+                         type = "tdb")$est
+    }
+  }
+
+  if(!is.null(tdp)){
+    e_a = 6.11 * 10^((7.5 * tdp)/(237.3 + tdp))
+  } else {
+    e_s = svp_calc(tdb,
+                   units = "mbar")$est
+    e_a = rh/100*e_s
+
+  }
+
+  e_a = press_convert(e_a,
+                      from = "mbar",
+                      to = units)$est
+
+  structure(list(est = e_a,
+                 type = "avp",
+                 meas = "Pressure",
+                 units = units),
+            class = "thermoreg_meas")
+  # 0.750062*mbar/mbarar -> mmHg
+  # 1/5171493256*mmHg -> psi
+  # 1/760
+}
+
+#' @describeIn pressure_calcs Calculate saturated vapor pressure
+#' @export
+svp_calc = function(tdb = NULL,
+                    temp = thermoreg_options("temp_scale"),
+                    units = thermoreg_options("press_scale")){
+
+  if(class(tdb) == "thermoreg_meas"){
+    tdb = tdb$est
+  }
+  if(temp != "celsisus" | temp != "c"){
+    if(!is.null(tdp)){
+      tdp = temp_convert(x = tdp,
+                         to = "celsius",
+                         from = temp,
+                         type = "tdp")$est
+    }
+  }
+  e_s = (6.11 * 10^((7.5 * tdb)/(237.3 + tdb)))
+
+  e_s = press_convert(e_s,
+                      to = units,
+                      from = "mbar")$est
+
+  structure(list(est = e_s,
+                 type = "svp",
+                 meas = "Pressure",
+                 units = units),
+            class = "thermoreg_meas")
+}
+
+
+
--- R/temp.R
+++ R/temp.R
@@ -43,3 +43,39 @@ temp_init = function(est = NULL,
 
 }
 
+
+#' @describeIn temperature_calcs Dew point calculation
+#' @export
+
+tdp_calc = function(tdb = NULL,
+                    rh = NULL,
+                    units = thermoreg_options("temp_scale")){
+  if(class(tdb) == "thermreg_meas"){
+    if(tdb$units != units){
+      stop("input temperature units from thermoreg_meas must match units argument")
+    }
+  } else if(class(tdb) == "numeric"){
+    tdb = temp_init(est = tdb,
+                    type = "tdb",
+                    units = units)
+  } else {
+    stop("tdp input must be thermoreg_meas or numeric")
+  }
+
+  x = temp_convert(x = tdb,
+                   to = "celsius")
+  # Magnus-Tetens formula
+  a = 17.62
+  b = 243.12
+  alpha_func = log(rh/100) + a*x$est/(b+x$est)
+  t2 = (b*alpha_func / (a - alpha_func))
+  t3 = temp_convert(x = t2,
+                    from = "celsius",
+                    to = units)
+  structure(list(est = t3$est,
+                 type = "tdp",
+                 meas = "Temperature",
+                 units = t3$units),
+            class = "thermoreg_meas")
+}
+
